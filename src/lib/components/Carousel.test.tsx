23f9865f888bedae35bdbb12324e78453f1890e1
Peter Ju <peterju35@gmail.com>
2021-06-15 00:50:17 -0400

    First unit test and tsconfig settings

---
+++ src/lib/components/Carousel.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+describe("<Carousel />", () => {
+  it("Should render", () => {
+    render(<Carousel />);
+  });
+});
